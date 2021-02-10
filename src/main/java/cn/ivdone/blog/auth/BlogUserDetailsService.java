b3f06df82d6adc600d1098e2bcf7a075afc22cff
helunhelun <47818862+helunhelun@users.noreply.github.com>
2021-02-10 12:52:26 +0800

    Update BlogUserDetailsService.java

--- src/main/java/cn/ivdone/blog/auth/BlogUserDetailsService.java
+++ src/main/java/cn/ivdone/blog/auth/BlogUserDetailsService.java
@@ -19,14 +19,22 @@ public class BlogUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         User user = userService.findUserByUsername(username) ;
         if (user == null){
             throw new BadCredentialsException("用户不存在") ;
         }
+
 //        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder() ;
 //        String encodedPassword = passwordEncoder.encode(user.getPassword().trim()) ;
 //        user.setPassword(encodedPassword);
 //        userService.savaUser(user);
+        // 没有管理员选项  普通用户没有管理员权限
+        if (!user.getIs_stuff()){
+            return new org.springframework.security.core.userdetails.User(username, user.getPassword(),
+                    AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")) ;
+        }
+        // 正常为管理员权限
         return new org.springframework.security.core.userdetails.User(username, user.getPassword(),
                 AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN")) ;
     }
